77700f722d7120cdbcd6a8f6f911261ad30522ff
cisco <vadonjb@gmail.com>
2022-10-06 10:08:22 +0200

    Initialisation du projet et mise en place des variables d'environnement

---
+++ config/bootstrap.php
@@ -0,0 +1,9 @@
+<?php
+
+// Chargement de l'autoloader de composer
+
+    require_once __DIR__ . "/../vendor/autoload.php";
+
+// chargement des variables d'environnement
+    $dotenv = Dotenv\Dotenv::createImmutable(dirname (__DIR__));
+    $dotenv->load();
